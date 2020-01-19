955328b1d38187829dbff93bff759a2c4e0326c3
Adam Storr <astorr@gmail.com>
2020-01-19 18:42:07 +0000

    Add in dependency injection project to set up the IServiceCollection setup

--- samples/AspNetCoreExample/Startup.cs
+++ samples/AspNetCoreExample/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EmailRep.NET;
+using EmailRep.NET.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -25,9 +26,7 @@ namespace AspNetCoreExample
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            var settings = Configuration.GetSection("EmailRepNet").Get<EmailRepClientSettings>();
-            services.AddSingleton(settings);
-            services.AddHttpClient<IEmailRepClient, EmailRepClient>();
+            services.AddEmailRep(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ src/EmailRep.NET.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmailRep.NET.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// ServiceCollection Extensions to setup the email rep registration
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Add email rep client and register associated settings through configuration for the client.
+        /// </summary>
+        /// <param name="services">The instance of <see cref="IServiceCollection"/> on which the services are to be registered with.</param>
+        /// <param name="configuration">The configuration section which contains the email rep configuration section.</param>
+        /// <param name="sectionName">The configuration section name to use. Default: "EmailRepNet".</param>
+        /// <returns></returns>
+        public static IServiceCollection AddEmailRep(this IServiceCollection services, IConfiguration configuration, string sectionName = "EmailRepNet")
+        {
+            var settings = new EmailRepClientSettings();
+            configuration.GetSection(sectionName).Bind(settings);
+            services.AddSingleton(settings);
+
+            services.AddHttpClient<IEmailRepClient, EmailRepClient>();
+            return services;
+        }
+    }
+}
